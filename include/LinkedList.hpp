19d20e10ff709152661a2f7daa3bff6cc71f275e
Ali <alimovmuhammadali01222008@gmail.com>
2025-11-18 10:18:19 +0500

    created linked list deletion methods

--- include/LinkedList.hpp
+++ include/LinkedList.hpp
@@ -49,4 +49,74 @@ public:
         newNode->next=temp->next;
         temp->next=newNode;   
     }
+
+    void delete_beginning(){
+        if (head==nullptr)
+        {
+            cout<<"Linked list is already empthy"<<endl;
+            return;
+        }
+        Node* temp=head;
+        head=head->next;
+        delete temp;
+        cout<<"Front Node is deleted"<<endl;
+    }
+
+    void delete_end(){
+        if (head==nullptr)
+        {
+            cout<<"Linked list is already empthy"<<endl;
+            return;
+        }
+
+        if(head->next==nullptr){
+            delete head;
+            head=nullptr;
+            cout<<"End Node is deleted"<<endl;
+        }
+
+        Node* temp=head;
+        while (temp->next->next!=nullptr) 
+        {
+            temp=temp->next;
+        }
+        delete temp->next;
+        temp->next=nullptr;
+        cout<<"End Node is deleted"<<endl;
+    }
+
+    void delete_middle(int value){
+        //delete by value
+        if (head==nullptr)
+        {
+            cout<<"Linked list is already empthy"<<endl;
+            return;
+        }
+
+        if (head->data==value)
+        {
+            Node* temp=head;
+            head=head->next;
+            delete temp;
+            cout<<"Selected Node is deleted"<<endl;
+            return;
+        }
+        Node* temp=head;
+        while (temp->next->data!=value && temp->next!=nullptr)
+        {
+            temp=temp->next;
+        }
+
+        if (temp->next==nullptr)
+        {
+            cout<<"No matching value found in linked list"<<endl;
+            return;
+        }
+
+        Node* toDelete=temp->next;
+        temp->next=temp->next->next;
+        delete toDelete;
+        cout<<"Selected Node is deleted"<<endl;
+    }
+
 };
